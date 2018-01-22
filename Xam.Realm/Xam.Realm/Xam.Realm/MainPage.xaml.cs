ad7198797ccb599a480857e956c522adb58ae34f
David malý <maly.david.it@gmail.com>
2018-01-22 14:35:23 +0100

    Added Realm support

--- Xam.Realm/Xam.Realm/Xam.Realm/MainPage.xaml.cs
+++ Xam.Realm/Xam.Realm/Xam.Realm/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Realms;
 
 namespace Xam.Realm
 {
@@ -13,5 +14,25 @@ namespace Xam.Realm
 		{
 			InitializeComponent();
 		}
+	    protected override void OnAppearing()
+	    {
+	        base.OnAppearing();
+	        var RealmDB = Realms.Realm.GetInstance();
+
+	        var vEmpId = RealmDB.All<Note>().Count() + 1;
+	        var vEmployee = new Note()
+	        {
+	            ID = vEmpId,
+                Text = "note text"
+	        };
+	        RealmDB.Write(() => {
+	            vEmployee = RealmDB.Add(vEmployee);
+	        });
+
+
+            var allNotes = RealmDB.All<Note>();
+	        notesListView.ItemsSource = allNotes;
+	    }
 	}
 }
+
---
+++ Xam.Realm/Xam.Realm/Xam.Realm/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realms;
+
+namespace Xam.Realm
+{
+    class Note: RealmObject
+    {
+        [PrimaryKey]
+        public long ID { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Text {Text}";
+        }
+    }
+}
